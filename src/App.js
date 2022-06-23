e1f0355546d97c5f63123c1c911e236059388d3e
Roberto Bressanelli <rbressanelli@gmail.com>
2022-06-23 17:37:59 -0300

    Feat: added projects cards

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import Header from "./components/Header";
 import GlobalStyle from "./global";
 import photo from "./assets/photo.png";
 import react from "./assets/react.svg";
-import html from "./assets/html.svg"
-import css from "./assets/css.svg"
-import javascript from "./assets/javascript.svg"
-import python from "./assets/python.svg"
-import django from "./assets/django.svg"
-import flask from './assets/flask.svg'
-import node from './assets/nodedotjs.svg'
-import heroku from './assets/heroku.svg'
-import express from './assets/express.svg'
+import html from "./assets/html.svg";
+import css from "./assets/css.svg";
+import javascript from "./assets/javascript.svg";
+import python from "./assets/python.svg";
+import django from "./assets/django.svg";
+import flask from "./assets/flask.svg";
+import node from "./assets/nodedotjs.svg";
+import heroku from "./assets/heroku.svg";
+import express from "./assets/express.svg";
+import lig4 from "./assets/lig4.webm";
+import space from "./assets/space.webm";
+import hamburger from './assets/hamburger.webm'
+import geek_legends from './assets/geek-legends.webm'
 
 import {
   Container,
   Description,
   ImageCard,
+  LinkDeploy,
   Main,
+  ProjectCard,
+  Projects,
   Section,
   Tecnologies,
+  VideoBackGround,
 } from "./styles";
 
 function App() {
@@ -43,20 +51,117 @@ function App() {
         </Section>
         <Section>
           <div>
-          <h2>Tecnologias</h2>
-          <Tecnologies>
-            <img src={react} alt="react" />
-            <img src={html} alt="html" />
-            <img src={css} alt="css" />
-            <img src={javascript} alt="javascript" />
-            <img src={python} alt="python" />
-            <img src={django} alt="django" />
-            <img src={flask} alt="flask" />
-            <img src={node} alt="node" />
-            <img src={heroku} alt="heroku" />
-            <img src={express} alt="express" />
-          </Tecnologies>
-          </div>        
+            <h2>Tecnologias</h2>
+            <Tecnologies>
+              <a
+                href="https://pt-br.reactjs.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={react} alt="react" />
+              </a>
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={html} alt="html" />
+              </a>
+
+              <a
+                href="https://developer.mozilla.org/pt-BR/docs/Web/CSS"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={css} alt="css" />
+              </a>
+              <a
+                href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={javascript} alt="javascript" />
+              </a>
+              <a
+                href="https://www.python.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={python} alt="python" />
+              </a>
+              <a
+                href="https://www.djangoproject.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={django} alt="django" />
+              </a>
+              <a
+                href="https://flask.palletsprojects.com/en/2.1.x/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={flask} alt="flask" />
+              </a>
+              <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer">
+                <img src={node} alt="node" />
+              </a>
+              <a
+                href="https://dashboard.heroku.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={heroku} alt="heroku" />
+              </a>
+              <a
+                href="https://expressjs.com/pt-br/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={express} alt="express" />
+              </a>
+            </Tecnologies>
+          </div>
+        </Section>
+        <Section>
+          <Projects>
+            <h2>Projetos</h2>
+            <div id='projects-container'>
+
+              <ProjectCard>
+                <h3>Game Lig4</h3>
+                <VideoBackGround autoPlay loop muted id="video">
+                  <source src={lig4} type="video/webm" />
+                </VideoBackGround>
+                <LinkDeploy >site</LinkDeploy>
+              </ProjectCard>
+
+              <ProjectCard>
+                <h3>Space Flight News</h3>
+                <VideoBackGround autoPlay loop muted id="video">
+                  <source src={space} type="video/webm" />
+                </VideoBackGround>
+                <LinkDeploy >site</LinkDeploy>
+              </ProjectCard>
+
+              <ProjectCard>
+                <h3>Hamburger Shop</h3>
+                <VideoBackGround autoPlay loop muted id="video">
+                  <source src={hamburger} type="video/webm" />
+                </VideoBackGround>
+                <LinkDeploy >site</LinkDeploy>
+              </ProjectCard>
+              
+              <ProjectCard>
+                <h3>Geek Legends</h3>
+                <VideoBackGround autoPlay loop muted id="video">
+                  <source src={geek_legends} type="video/webm" />
+                </VideoBackGround>
+                <LinkDeploy >site</LinkDeploy>
+              </ProjectCard>
+
+            </div>
+          </Projects>
         </Section>
       </Main>
       <Footer></Footer>
--- src/styles.js
+++ src/styles.js
@@ -10,58 +10,112 @@ export const Main = styled.main`
   padding: 50px 0;
 `;
 
-
 export const Section = styled.section`
   display: flex;
   justify-content: space-around;
 `;
 
-
 export const ImageCard = styled.div`
-    padding: 20px;
-    display: flex;
-    justify-content: center;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
 
-    img {
-        width: 250px;
-        height: 250px;
-        border: 4px solid #aaa;
-        border-radius: 100%;
-        object-fit: cover;
+  img {
+    width: 250px;
+    height: 250px;
+    border: 4px solid #aaa;
+    border-radius: 100%;
+    object-fit: cover;
 
-        @media only screen and (min-width: 800px) {
-            :hover {                
-                transform: scale(1.02); 
-                transition: 500ms;
-            }
-        }
+    @media only screen and (min-width: 800px) {
+      :hover {
+        transform: scale(1.02);
+        transition: 500ms;
+      }
     }
+  }
 `;
 
 export const Description = styled.div`
-    padding: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    max-width: 400px;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 400px;
 `;
 
-
 export const Tecnologies = styled.div`
-    padding: 20px;
-    display: flex;
-    justify-content: center;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
 
-    img {
-        width: 50px;
-        height: 50px;       
-        margin: 10px;        
+  img {
+    width: 50px;
+    height: 50px;
+    margin: 10px;
 
-        @media only screen and (min-width: 800px) {
-            :hover {                
-                transform: scale(1.05); 
-                transition: 300ms;
-            }
-        }
+    @media only screen and (min-width: 800px) {
+      :hover {
+        transform: scale(1.05);
+        transition: 300ms;
+      }
     }
+  }
+`;
+
+export const VideoBackGround = styled.video`
+  width: 200px;
+  object-fit: cover;
+`;
+
+export const Projects = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div#projects-container {
+    display: flex;
+    flex-direction: row;
+  }
+`;
+
+export const ProjectCard = styled.div`
+  margin: 20px;
+  padding: 10px;
+  border: 2px solid #000;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+
+  h3 {
+    margin: 10px 0;
+  }
+`;
+
+export const LinkDeploy = styled.button`
+  margin: 20px 0;
+  width: 100%;
+  height: 30px;
+  border: transparent;
+  border-radius: 5px;
+
+  background-image: linear-gradient(
+    to right,
+    #282e7b 0%,
+    #4889ea 51%,
+    #282e7b 100%
+  );
+
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: #EBEBEB;
+  box-shadow: 0 0 15px 0px #868b8e;
+  display: block;
+
+  :hover {
+    background-position: right center;
+    color: #fff;
+    text-decoration: none;
+  }
 `;
